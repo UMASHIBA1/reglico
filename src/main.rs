a38c1ea48af7f7b4ae301779d5c39678bbca24d4
UMASHIBA <umashiba1@gmail.com>
2021-07-28 00:58:13 +0900

    mod: write grammer with pest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
+extern crate once_cell;
+#[macro_use]
+extern crate lazy_static;
 
 mod lexer;
 mod parser;
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,19 +1,47 @@
 use super::ast;
 use crate::parser::ast::AstNode;
 use std::fmt::Error;
+use once_cell::sync::Lazy;
+use lazy_static::lazy_static;
 
 use pest::Parser;
-use pest::iterators::Pairs;
+use pest::iterators::{Pairs, Pair};
+use pest::prec_climber::*;
 
 #[derive(Parser)]
 #[grammar = "parser/reglico.pest"]
 struct ReglicoParser;
 
-pub fn parse(source: &str) -> Result<Pairs<'_, Rule>, pest::error::Error<Rule>> {
-    // let mut ast = vec![];
+lazy_static! {
+    static ref PREC_CLIMBER: PrecClimber<Rule> = {
+    use Rule::*;
+    use Assoc::*;
+    PrecClimber::new(vec![
+            Operator::new(Rule::),
+        Operator::new(Rule::verb, Left)
+    ])
+};
+}
 
-    let pairs = ReglicoParser::parse(Rule::const_assignment_expr, source);
-    pairs
+pub fn eval(program: Pairs<Rule>) -> () {
+    PREC_CLIMBER.climb(program,
+                       |pair: Pair<Rule>| match pair.as_rule() {
+                           _ => pair
+                       },
+                       |lhs: f64, op: Pair<Rule>, rhs: f64| match op.as_rule() {
+                           _ => unreachable!(),
+                       },
+    );
+}
+
+pub fn parse(source: &str) -> () {
+
+    let parse_result = ReglicoParser::parse(Rule::program, source);
+    match parse_result {
+        Ok(program) => eval(program),
+        Err(_) => println!("Syntax Error!")
+    }
+
 }
 
 
@@ -25,6 +53,21 @@ mod tests {
     #[test]
     fn const_assign_test() {
 
-        assert_eq!(parse("const bbb = 90"), parse("const aaa = 10"))
+        assert_eq!(parse("
+    fn add(a: number, b: number) {
+        return
+        a + b;
+    }
+
+    const total = add(1,2);
+    "), parse("
+        fn mul(a: number, b: number) {
+        return
+        a + b;
+    }
+
+    const total = add(3,2);
+    "))
+
     }
 }
