bc56997ecd3d8bf3743fa18958bf7c662806dc8f
UMASHIBA <umashiba1@gmail.com>
2021-08-02 00:23:12 +0900

    mod: add test

--- src/parser2/parser.rs
+++ src/parser2/parser.rs
@@ -4,12 +4,22 @@ mod test {
 
 
     #[test]
-    fn test_assignment_with_type() {
+    fn test_const_assignment_with_type() {
         lalrpop_mod!(pub reglico);
 
         let expr = reglico::ConstAssignmentParser::new().parse("const tmp1: number = 10").unwrap();
 
         assert_eq!(&format!("{:?}", expr), "const tmp1: number = 10");
+
+    }
+
+    #[test]
+    fn test_const_assignment_no_type() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ConstAssignmentParser::new().parse("const tmp1 = 10").unwrap();
+
+        assert_eq!(&format!("{:?}", expr), "const tmp1 = 10");
 
     }
 
