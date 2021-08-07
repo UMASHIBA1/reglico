52c4e106cef474789f3f52933d91cd7c27ac7c1e
UMASHIBA <umashiba1@gmail.com>
2021-08-08 02:28:07 +0900

    mod: can parse function

--- src/parser2/ast.rs
+++ src/parser2/ast.rs
@@ -11,6 +11,31 @@ pub enum Types {
 }
 
 #[derive(Debug)]
+pub struct FuncArg {
+    pub name: String,
+    pub argType: Types,
+}
+
+#[derive(Debug)]
+pub struct Func {
+    pub name: String,
+    pub args: Vec<FuncArg>,
+    pub stmts: Vec<Stmt>,
+}
+
+#[derive(Debug)]
 pub enum Expr {
     Number(i32),
+}
+
+#[derive(Debug)]
+pub struct ExprStmt {
+    pub expr: Expr,
+}
+
+#[derive(Debug)]
+pub enum Stmt {
+    VariableDeclaration(VariableDeclaration),
+    ExprStmt(ExprStmt),
+    Func(Func),
 }
--- src/parser2/parser.rs
+++ src/parser2/parser.rs
@@ -7,9 +7,9 @@ mod test {
     fn test_const_assignment_with_type() {
         lalrpop_mod!(pub reglico);
 
-        let expr = reglico::ConstAssignmentParser::new().parse("const tmp1: number = 10").unwrap();
+        let expr = reglico::StmtParser::new().parse("const tmp1: number = 10;").unwrap();
 
-        assert_eq!(&format!("{:?}", expr), "const tmp1: number = 10");
+        assert_eq!(&format!("{:?}", expr), "const tmp1: number = 10;");
 
     }
 
@@ -17,11 +17,26 @@ mod test {
     fn test_const_assignment_no_type() {
         lalrpop_mod!(pub reglico);
 
-        let expr = reglico::ConstAssignmentParser::new().parse("const tmp1 = 10").unwrap();
+        let expr = reglico::StmtParser::new().parse("const tmp1 = 10;").unwrap();
 
-        assert_eq!(&format!("{:?}", expr), "const tmp1 = 10");
+        assert_eq!(&format!("{:?}", expr), "const tmp1 = 10;");
+    }
 
+    #[test]
+    fn test_function() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ProgramParser::new().parse("fn add(a: number, b: number) {
+        const tmp1 = 32;
+        3;
+        fn func1(){
+            const tmp2 = 1;
+            2;
+        }
+
+        }").unwrap();
+
+        assert_eq!(&format!("{:?}", expr), "tmp");
     }
 
 }
-
