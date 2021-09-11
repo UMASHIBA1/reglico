06aa82ba63d3169e6d912a55999b9e500f03ade0
UMASHIBA <umashiba1@gmail.com>
2021-09-12 00:23:38 +0900

    mod: create tests for mul and div calc

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -321,6 +321,83 @@ mod test {
         assert_eq!(expr, expected_expr);
     }
 
+
+    #[test]
+    fn test_op_mul_and_mul() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ProgramParser::new().parse("8 * 2 * 3;").unwrap();
+
+        let expected_expr = vec![
+            Stmt::expr_new(
+                Expr::op_new(
+                    Expr::op_new(Expr::num_new(8), Opcode::Mul, Expr::num_new(2)),
+                    Opcode::Mul,
+                    Expr::num_new(3),
+                )
+            )
+        ];
+
+        assert_eq!(expr, expected_expr);
+    }
+
+    #[test]
+    fn test_op_mul_and_div() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ProgramParser::new().parse("3 * 2 / 3;").unwrap();
+
+        let expected_expr = vec![
+            Stmt::expr_new(
+                Expr::op_new(
+                    Expr::op_new(Expr::num_new(3), Opcode::Mul, Expr::num_new(2)),
+                    Opcode::Div,
+                    Expr::num_new(3),
+                )
+            )
+        ];
+
+        assert_eq!(expr, expected_expr);
+    }
+
+    #[test]
+    fn test_op_div_and_mul() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ProgramParser::new().parse("6 / 2 * 3;").unwrap();
+
+        let expected_expr = vec![
+            Stmt::expr_new(
+                Expr::op_new(
+                    Expr::op_new(Expr::num_new(6), Opcode::Div, Expr::num_new(2)),
+                    Opcode::Mul,
+                    Expr::num_new(3),
+                )
+            )
+        ];
+
+        assert_eq!(expr, expected_expr);
+    }
+
+    #[test]
+    fn test_op_div_and_div() {
+        lalrpop_mod!(pub reglico);
+
+        let expr = reglico::ProgramParser::new().parse("8 / 2 / 4;").unwrap();
+
+        let expected_expr = vec![
+            Stmt::expr_new(
+                Expr::op_new(
+                    Expr::op_new(Expr::num_new(8), Opcode::Div, Expr::num_new(2)),
+                    Opcode::Div,
+                    Expr::num_new(4),
+                )
+            )
+        ];
+
+        assert_eq!(expr, expected_expr);
+    }
+
     #[test]
     fn test_call() {
         lalrpop_mod!(pub reglico);
