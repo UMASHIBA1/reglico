349431f70ccc88d9635d9dd92ce17820ec2db3a8
UMASHIBA <umashiba1@gmail.com>
2021-10-04 15:16:24 +0900

    mod: add tests for if_stmt to_rust

--- src/to_ts_rust/to_rust/if_stmt/if_stmt.rs
+++ src/to_ts_rust/to_rust/if_stmt/if_stmt.rs
@@ -24,12 +24,89 @@ impl ToRust {
 
         match else_stmt_str {
             Some(else_stmt_str) => {
-                format!("if {}{}else{}", condition_expr_str, then_stmt_str, else_stmt_str)
+                format!("if {} {}else {}", condition_expr_str, then_stmt_str, else_stmt_str)
             },
             None => {
-                format!("if {}{}", condition_expr_str, then_stmt_str)
+                format!("if {} {}", condition_expr_str, then_stmt_str)
             }
         }
 
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::type_parser::typed_ast::{TypedStmt, TypedExpr, TypedBlockBox, TypedCanElseStmt};
+    use crate::to_ts_rust::to_rust::to_rust::ToRust;
+
+    #[test]
+    fn test_if_stmt() {
+        let typed_stmts = vec![TypedStmt::if_stmt_new(
+            TypedExpr::bool_expr_new(true),
+            TypedBlockBox::new(
+                vec![TypedStmt::expr_new(TypedExpr::num_expr_new(1))],
+                None
+            ),
+            None,
+            None
+        )];
+
+        let rust_code = ToRust::to_rust(typed_stmts, None);
+
+        let expected_rust_code = "if true {1;}";
+
+        assert_eq!(rust_code, expected_rust_code);
+
+    }
+
+    #[test]
+    fn test_if_else_block_box_stmt() {
+        let typed_stmts = vec![TypedStmt::if_stmt_new(
+            TypedExpr::bool_expr_new(true),
+            TypedBlockBox::new(vec![TypedStmt::expr_new(TypedExpr::num_add_new(
+                TypedExpr::num_expr_new(1),
+                TypedExpr::num_expr_new(2)
+            ))], None),
+            Some(TypedCanElseStmt::block_box_new(
+                vec![TypedStmt::expr_new(TypedExpr::num_expr_new(1))],
+                None
+            )),
+            None
+        )];
+
+
+        let rust_code = ToRust::to_rust(typed_stmts, None);
+
+        let expected_rust_code = "if true {1+2;}else {1;}";
+
+        assert_eq!(rust_code, expected_rust_code);
+
+    }
+
+    #[test]
+    fn test_if_elseif_stmt() {
+        let typed_stmts = vec![TypedStmt::if_stmt_new(
+            TypedExpr::bool_expr_new(true),
+            TypedBlockBox::new(vec![TypedStmt::expr_new(TypedExpr::num_add_new(
+                TypedExpr::num_expr_new(1),
+                TypedExpr::num_expr_new(2)
+            ))], None),
+            Some(TypedCanElseStmt::if_stmt_new(
+                TypedExpr::bool_expr_new(true),
+                TypedBlockBox::new(vec![TypedStmt::expr_new(TypedExpr::num_expr_new(1))], None),
+                None,
+                None
+            )),
+            None
+        )];
+
+
+        let rust_code = ToRust::to_rust(typed_stmts, None);
+
+        let expected_rust_code = "if true {1+2;}else if true {1;}";
+
+        assert_eq!(rust_code, expected_rust_code);
+
+    }
+
 }
