4d8206ce6eca4e4d713ffb738625b0b77850d9a4
UMASHIBA <umashiba1@gmail.com>
2021-10-08 22:03:11 +0900

    add: create setup.sh

--- output/ts_output/output.ts
+++ output/ts_output/output.ts
@@ -1,1 +1,1 @@
-const add=(a:number,b:number):number=>{return a+b;}const total=add(1,2);
+const add=(a:number,b:number):number=>{return a+b;};const total=add(1,2);
---
+++ setup.sh
@@ -0,0 +1,5 @@
+cd output
+npm install
+cd ts_output
+npm install
+cd ../..
