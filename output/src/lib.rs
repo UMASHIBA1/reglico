48805186e3083128e06f971c0874401e63167715
UMASHIBA <umashiba1@gmail.com>
2021-10-09 13:08:00 +0900

    mod: change transpiled rust code

--- output/src/lib.rs
+++ output/src/lib.rs
@@ -4,6 +4,6 @@ use wasm_bindgen::prelude::*;use web_sys::console;#[cfg(feature = "wee_alloc")]#
     pub fn main_js() -> Result<(), JsValue> {
         #[cfg(debug_assertions)]
         console_error_panic_hook::set_once();
-    fn add(a:f32,b:f32)->f32{a+b}let total=add(1,2);
+    fn add(a:f32,b:f32)->f32{a+b}let total=add(1.0,2.0);
     Ok(())
     }
